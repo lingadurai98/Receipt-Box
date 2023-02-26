f480d79111e3be107a1fd85bd15aba09cb5563d6
lingadurai98 <LIngadurai.Ponnudurai@detrans.io>
2023-02-26 23:23:44 +0530

    reupdate 2

--- App.js
+++ App.js
@@ -5,40 +5,11 @@ import Input from "./components/input/Input";
 import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
-  const headerData = {
-    heading: "Login",
-    text: "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy",
-  };
-  const blurHandler = () => {
-    Alert.alert("summa", "summa panna", [{ text: "ok" }]);
-  };
   return (
-    <LinearGradient style={styles.container} colors={["#d2f5eb", "#026838"]}>
-      <Header heading={headerData.heading} text={headerData.text} />
-      <Input
-        placeholder="enter your name"
-        errMsg="Please Enter the Valid user Name"
-        secureTextEntry={true}
-        type={true}
-        // onBlur={blurHandler}
-      />
-      <Input
-        placeholder="enter your name"
-        errMsg="Please Enter the Valid user Name"
-        secureTextEntry={true}
-        type={true}
-        isError={true}
-        // onBlur={blurHandler}
-      />
-      <Input
-        placeholder="enter your name"
-        errMsg="Please Enter the Valid user Name"
-        secureTextEntry={true}
-        type={true}
-        // onBlur={blurHandler}
-      />
-      <Button>Submit</Button>
-    </LinearGradient>
+    <LinearGradient
+      style={styles.container}
+      colors={["#d2f5eb", "#026838"]}
+    ></LinearGradient>
   );
 }
 
