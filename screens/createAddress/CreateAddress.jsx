484bd6f8cbe8e5ec552c54f359dedc10ae0e461d
lingadurai98 <LIngadurai.Ponnudurai@detrans.io>
2023-02-26 23:20:28 +0530

    commponent reupdate

---
+++ screens/createAddress/CreateAddress.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+function CreateAddress() {
+  return (
+    <View>
+      <Text>Create New Address </Text>
+    </View>
+  );
+}
+
+export default CreateAddress;
